5750a1d23389f67fb5c7d7887468d362f0a7b2ea
fasabet@gmail.com <fasabet@gmail.com>
2017-11-02 15:20:21 +0100

    board

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+    <script> 
+            
+            function Insert(m)
+            
+            {   alert("kkkk")
+               console.log (m);
+           }
+    </script>
+    
+    <style>
+            div{
+                background-color: pink;
+                width: 500px;
+                border-style: solid;
+                border-color: gray;
+                border-width: 2px;
+            }
+            
+            
+            button{
+                
+                background-color: white; /* Green */
+                width: 20px;
+                border: solid;
+                color: green;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+            }
+            form{
+                
+            }
+            
+        </style>
+</head>
+
+
+    <body>
+        
+        <div>
+                <!--input type=text id="idinput"-->
+                 <?php
+        $toetsen = array(array("","","","","","",""),
+                    array("","","","","","",""),
+                    array("","","","","","",""),
+                    array("","","","","","",""),
+                    array("","","","","","",""),
+                    array("","","","","","",""));
+        
+        
+        for ($x = 0; $x < 6; $x++) {
+             echo "<br>";
+            for ($y = 0; $y < 7; $y++) {
+                //$id= "$x.$y";      //    "1.2"
+               $id= $x."".$y;      //    "12"
+                //echo "\n";
+                echo '<button id=idbutton'.$id.' onclick="Insert(\''.$id.'\')">'.$toetsen[$x][$y].'</button>';
+            }
+        }
+        
+        
+        ?>
+                
+            
+        </div>
+       <!--input type="text" id="tempStorePlus"-->
+
+
+
+
+
+    </body>   
+</html>
